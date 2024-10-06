29ecc229e1fde81c40f5b4ad626599dad864cb70
Mackenzie Bowes <mackenzie@mackenziebowes.com>
2024-10-06 12:31:30 -0700

    create and return tokens after registration

--- src/routes/user/create.ts
+++ src/routes/user/create.ts
@@ -3,6 +3,7 @@ import prisma from "@/lib/prisma";
 import argon2 from "argon2";
 import { z } from "zod";
 import { validateSchema } from "@/middlewares/validateSchema";
+import { generateTokens } from "@/lib/utils/Token";
 
 const createUserSchema = z.object({
 	email: z.string(),
@@ -20,7 +21,22 @@ export const post = [
 					passwordHash,
 				},
 			});
+
 			if (createdUser) {
+				const tokens = generateTokens(createdUser.id);
+				const SEVEN_DAYS = 1000 * 60 * 60 * 24 * 7;
+				// Set tokens as HttpOnly cookies
+				res.cookie("httpToken", tokens.httpToken, {
+					httpOnly: true,
+					secure: true, // Set to true if using HTTPS
+					maxAge: SEVEN_DAYS, // 1 hour in milliseconds
+				});
+
+				res.cookie("wsToken", tokens.wsToken, {
+					httpOnly: true,
+					secure: true,
+					maxAge: SEVEN_DAYS,
+				});
 				return res.status(200).json({ error: null, result: createdUser });
 			}
 		} catch (error) {
